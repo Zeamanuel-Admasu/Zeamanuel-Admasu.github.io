94b96d30f83239aab44f282de63a6b1d3d756150
Zeamanuel-Admasu <zeamanshibru2020@gmail.com>
2025-10-05 16:06:10 +0300

    chore: static export + GH Pages workflow

---
+++ next.config.js
@@ -0,0 +1,6 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',              // build to ./out as static HTML
+  images: { unoptimized: true }, // make <Image> work without the Next server
+};
+module.exports = nextConfig;
